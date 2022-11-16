6c3f1d973fbf07ecb54521601946aeefa60c6af2
irshad007 <irshadmuhammedch8@gmail.com>
2022-11-16 12:47:29 +0530

    q1 corrected

---
+++ Question_1.py
@@ -0,0 +1,6 @@
+
+import xml.dom.minidom as dm
+def get_data(tag_name,attribute_name=None):
+    
+    doc = dm.parse("Data/library.xml")
+    pass
--- Question_2.py
+++ Question_2.py
@@ -15,32 +15,39 @@ def sort_and_pricess_the_string(input_string):
     # print(upper_dict)
     
     string_list=input_string.split(" ")
-    def find_weight_and_swap(s):
+    def find_weight(s):
         weight=0
-        swaped=''
         for i in s:
             if i.isupper() == True:
                 weight+=upper_dict[i]
-                swaped += i.lower()
             else:
                 weight+=lower_dict[i]
-                swaped+=i.upper()
         
-        return weight,swaped
+        return weight
     
     weighted_dict={}
     
     for i in string_list:
-        weight,swaped=find_weight_and_swap(i)
-        weighted_dict[weight] = swaped
+        weight=find_weight(i)
+        weighted_dict[weight] = i
         
     # print(weighted_dict)
     sorted_weight=sorted(list(weighted_dict.keys()),reverse=True)
     # print(sorted_weight)
     out = ''
     for i in sorted_weight:
-        out+=" "+weighted_dict[i]
+        out+=weighted_dict[i]+" "
+        
+    # print(out)
+    last_out=''
     
-    return out
+    for i,j in zip(input_string,out):
+        # print(i+"-"+j)
+        if i.isupper() == True:
+            last_out+=j.upper()
+        else:
+            last_out+=j.lower()
+    
+    return last_out
 
-print(sort_and_pricess_the_string("aBC DeF"))
+print(sort_and_pricess_the_string("aBcd EFGh IJKL"))
